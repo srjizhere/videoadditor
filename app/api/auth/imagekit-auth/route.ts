8f0070c4a4288d61f018d70c4693c0a5457e5841
suraj patil <bhiapatil@gmail.com>
2025-09-02 01:22:43 +0530

    routes

---
+++ app/api/auth/imagekit-auth/route.ts
@@ -0,0 +1,23 @@
+import { getUploadAuthParams } from "@imagekit/next/server"
+
+export async function GET() {
+
+    try {
+        const authenticationParameters = getUploadAuthParams({
+            privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string, // Never expose this on client side
+            publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
+    
+        })
+    
+        return Response.json({ 
+            authenticationParameters,
+            publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY
+         });
+    } catch (error) {
+        return Response.json({ 
+            error:"Authentication for imagekit failed"
+         },{
+            status:500
+         });
+    }
+}
---
+++ app/api/video/route.ts
@@ -0,0 +1,66 @@
+import Video, { IVideo } from "@/app/models/Video";
+import { authOptions } from "@/lib/auth";
+import { connectToDateBase } from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        await connectToDateBase()
+        const videos = await Video.find({}).sort({ createdAt: -1 }).lean();
+        if (!videos || videos.length === 0) {
+            return NextResponse.json([], { status: 200 })
+        }
+        return NextResponse.json(videos)
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({ error: "Failed to fetch videos" }, { status: 500 });
+    }
+};
+
+
+export async function POST(request: NextRequest) {
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session) {
+            return NextResponse.json({
+                error: "Unauthorized"
+            },
+                { status: 401 }
+            );
+
+        }
+
+        await connectToDateBase();
+        const body: IVideo = await request.json();
+        if (
+            !body.title ||
+            !body.desciption ||
+            !body.videoUrl ||
+            !body.thumbnailUrl
+        ) {
+            return NextResponse.json(
+                { error: "Missing required fields" },
+                { status: 400 }
+            );
+        }
+
+        const videoData = {
+            ...body,
+            controls: body.controls ?? true,
+            transformation: {
+                height: 1920,
+                width: 1080,
+                quality: body.transformation?.quality ?? 100
+            }
+        }
+
+        const newVideo = await Video.create(videoData);
+        return NextResponse.json(newVideo);
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Failed to create video" },
+            { status: 500 }
+        );
+    }
+}
